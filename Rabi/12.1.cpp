911ffa678e3d3e388e4d972ebf09b2a758afda55
Nikita_Smerti_SS+_Class <lpupti@gmail.com>
2026-01-15 10:19:25 +0300

    end

--- Rabi/12.1.cpp
+++ Rabi/12.1.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main() {
+	system("chcp 1251");
+	char name[30], sure_name[30];
 
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "Введите фамилию: ";
+	std::cin >> sure_name;
+	std::cout << "Приветствую, " << sure_name << ' ' << name;
 }
