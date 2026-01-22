730fc7065a12ab19b85b2eef89ce9fbceaf8cd2e
Nikita_Smerti_SS+_Class <lpupti@gmail.com>
2026-01-22 10:22:25 +0300

    end

--- Rabi/12.6.cpp
+++ Rabi/12.6.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
 
 int main() {
+	system("chcp 1251 > nul");
+	char name[30], password[30], password_repit[30], login[30], password_login[30];
+
+	std::cout << "Имя: ";
+	std::cin >> name;
+	std::cout << "Пароль: ";
+	std::cin >> password;
+	std::cout << "Повторите пароль: ";
+	std::cin >> password_repit;
+	while(strcmp(password, password_repit) != 0){
+	std::cout << "Повторите пароль: ";
+	std::cin >> password_repit;
+	}
+	std::cout << "Вы зарегались\n";
+
+	std::cout << "------------------------------------------------------\n";
+	std::cout << "Введите логин: ";
+	std::cin >> login;
+	std::cout << "Введите пароль: ";
+	std::cin >> password_login;
+	std::cout << "------------------------------------------------------\n";
+
+	while (strcmp(name, login) != 0 or strcmp(password, password_login) != 0) {
+		system("cls");
+		std::cout << "------------------------------------------------------\n";
+		std::cout << "Введите логин: ";
+		std::cin >> login;
+		std::cout << "Введите пароль: ";
+		std::cin >> password_login;
+		std::cout << "------------------------------------------------------\n";
+	}
+	std::cout << "Вы вошли\n";
+
+
 
 }
